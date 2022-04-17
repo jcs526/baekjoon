21a208914087bd898215789cdd2e06be7b90dea6
ChanSol <mutongzang@gmail.com>
2022-04-18 07:33:05 +0900

    220408 / 10926-??!, 10430-나머지

---
+++ src/bronze/No10926.java
@@ -0,0 +1,15 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class No10926 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String id =scanner.nextLine();
+
+        String result =id+"??!";
+
+        System.out.println(result);
+    }
+}
