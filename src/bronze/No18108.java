6bac139f94581032f24551c24a96438e48686519
ChanSol <mutongzang@gmail.com>
2022-04-19 18:50:35 +0900

    220408 / 18108 - 1998년생인 내가 태국에서는 2541년생?!, 1330 - 두수 비교하기, 9498 - 시험성정

---
+++ src/bronze/No18108.java
@@ -0,0 +1,15 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class No18108 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int year =scanner.nextInt();
+
+        int result = year-543;
+
+        System.out.println(result);
+    }
+}
