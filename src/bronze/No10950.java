e2eefe7d640dfdc31fbdeb0cfb696177bb8e7078
ChanSol <mutongzang@gmail.com>
2022-04-21 20:36:20 +0900

    220421 / 2793-구구단,  10950-A+B-3, 8393-합

---
+++ src/bronze/No10950.java
@@ -0,0 +1,16 @@
+package bronze;
+import java.util.Scanner;
+
+public class No10950 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+            System.out.println(A+B);
+        }
+    }
+}
