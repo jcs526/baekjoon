eb4a591a6ed31c6eee47652dd8dbe56083b35936
ChanSol <mutongzang@gmail.com>
2022-04-22 19:51:45 +0900

    220422 / 15552-빠른 A+B

---
+++ src/bronze/No15552.java
@@ -0,0 +1,24 @@
+package bronze;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class No15552 {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        for (int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine()," ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            bw.write(a+b+"\n");
+        }
+        bw.flush();
+
+    }
+}
