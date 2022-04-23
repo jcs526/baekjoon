5fc183b00d8790f6f93b01e7377c951a182bc62c
ChanSol <mutongzang@gmail.com>
2022-04-23 21:12:22 +0900

    220423 / 2741-N찍기, 2742-기찍N, 11021-A+B_7, 11022-A+B_8

---
+++ src/bronze/No11022.java
@@ -0,0 +1,17 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class No11022 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        for(int i=1;i<=n;i++){
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            System.out.println("Case #"+i+": "+a+" + "+b+" = "+(a+b));
+        }
+    }
+}
---
+++ src/bronze/No2741.java
@@ -0,0 +1,14 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class No2741 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        for (int i=1;i<=n;i++)
+            System.out.println(i);
+    }
+}
