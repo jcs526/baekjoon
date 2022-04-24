21e96cd4eb1810c36e980a202bdeffe5517386d0
ChanSol <mutongzang@gmail.com>
2022-04-24 23:25:01 +0900

    220424 / 2438-별찍기, 2439-별찍기2, 10871-X보다작은수

---
+++ src/bronze/No2438.java
@@ -0,0 +1,21 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class No2438 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for(int k=n-i;k>0;k--){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
