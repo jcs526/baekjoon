af2fb7e2b3245b2339c6649b1ed40a573ddd5048
ChanSol <mutongzang@gmail.com>
2022-05-01 22:44:34 +0900

    220501 / 1110-더하기 사이클

---
+++ src/bronze/No1110.java
@@ -0,0 +1,40 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class No1110 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = scanner.nextInt();
+
+        int N1,N2,N3;
+        int cnt = 0;
+
+
+        N1 =num;
+        while (true){
+            cnt++;
+
+//            System.out.println("N1 : "+N1);
+            if(N1>=10) {
+                N2 = N1 / 10 + N1 % 10;
+            }else {
+                N2 =N1;
+            }
+//            System.out.println("N2 : "+N2);
+
+                N3 = N1%10*10+N2%10;
+
+//            System.out.println("N3 : "+N3);
+           if(N3==num) {
+               System.out.println(cnt);
+               break;
+           }else
+//               System.out.println("num="+num+", N3="+N3);
+//               System.out.println("실패"+cnt);
+               N1=N3;
+
+        }
+    }
+}
