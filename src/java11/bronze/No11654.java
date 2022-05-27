d16f9ca533deeda310c2e4d9efab0950705f6f68
ChanSol <mutongzang@gmail.com>
2022-05-28 03:31:34 +0900

    220528 / 11654 - 아스키 코드

---
+++ src/java11/bronze/No11654.java
@@ -0,0 +1,17 @@
+package java11.bronze;
+
+import java.util.Scanner;
+
+public class No11654 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String str = scanner.next();
+
+        int num = str.charAt(0);
+
+        System.out.println(num);
+
+
+    }
+}
